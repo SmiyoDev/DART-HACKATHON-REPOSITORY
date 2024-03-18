db9caa9dd855eb0646dd9fce8c927eb6acef0f36
Kelvin Mwanyumba <smithsmiyo@gmail.com>
2024-03-18 18:37:39 +0300

    New changes

--- functions.dart
+++ functions.dart
@@ -1,2 +1,21 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+void main() {
+  int num1 = 16;
+  int num2 = 32;
+
+  int sumResult = sum(num1, num2);
+  int multiplyResult = multiply(num1, num2);
+
+  print('The result of adding $num1 and $num2 is: $sumResult');
+  print('The result of multiplying $num1 and $num2 is: $multiplyResult');
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
+
+// Program 2: Perform Mathematical Operations with Functions
+//Write a Dart program that performs two mathematical operations using functions.
--- program1..dart
+++ program1..dart
@@ -1,2 +1,10 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+void main() {
+  String name = 'Kelvin Mwanyumba';
+  int age = 26;
+  String school = 'Power Learn Project';
+  String hobby = 'reading and watching movies';
+
+  print('My name is $name, I am $age years old, I attend $school, and I enjoy $hobby.');
+}
+//Program 1: Display Personal Information
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
